2bec8a28db3ef3e26f153f6eb236ef526f34ab6f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-30 00:46:29 +0100

    Play with js-quantities

--- grammar.js
+++ grammar.js
@@ -4,20 +4,19 @@
 function id(x) {return x[0]; }
 var grammar = {
     ParserRules: [
-    {"name": "main", "symbols": ["AS"], "postprocess": d => d[0]},
+    {"name": "AS", "symbols": ["AS", {"literal":"+"}, "MD"], "postprocess": d => d[0] + d[2]},
+    {"name": "AS", "symbols": ["AS", {"literal":"-"}, "MD"], "postprocess": d => d[0] - d[2]},
+    {"name": "AS", "symbols": ["MD"], "postprocess": d => d[0]},
     {"name": "P", "symbols": [{"literal":"("}, "AS", {"literal":")"}], "postprocess": d => d[1]},
     {"name": "P", "symbols": ["N"], "postprocess": d => d[0]},
     {"name": "MD", "symbols": ["MD", {"literal":"*"}, "P"], "postprocess": d => d[0] * d[2]},
     {"name": "MD", "symbols": ["MD", {"literal":"/"}, "P"], "postprocess": d => d[0] / d[2]},
     {"name": "MD", "symbols": ["P"], "postprocess": d => d[0]},
-    {"name": "AS", "symbols": ["AS", {"literal":"+"}, "MD"], "postprocess": d => d[0] + d[2]},
-    {"name": "AS", "symbols": ["AS", {"literal":"-"}, "MD"], "postprocess": d => d[0] - d[2]},
-    {"name": "AS", "symbols": ["MD"], "postprocess": d => d[0]},
     {"name": "N$ebnf$1", "symbols": [/[0-9]/]},
     {"name": "N$ebnf$1", "symbols": ["N$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "N", "symbols": ["N$ebnf$1"], "postprocess": d => parseInt(d[0].join(''), 10)}
 ]
-  , ParserStart: "main"
+  , ParserStart: "AS"
 }
 if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
    module.exports = grammar;
--- grammar.ne
+++ grammar.ne
@@ -1,4 +1,8 @@
-main -> AS {% d => d[0] %}
+#Addition and subtraction
+AS ->
+    AS "+" MD {% d => d[0] + d[2] %}
+  | AS "-" MD {% d => d[0] - d[2] %}
+  | MD {% d => d[0] %}
 
 #Parens
 P -> "(" AS ")" {% d => d[1] %}
@@ -8,12 +12,6 @@ P -> "(" AS ")" {% d => d[1] %}
 MD -> MD "*" P {% d => d[0] * d[2] %}
     | MD "/" P {% d => d[0] / d[2] %}
     | P {% d => d[0] %}
-
-#Addition and subtraction
-AS ->
-    AS "+" MD {% d => d[0] + d[2] %}
-  | AS "-" MD {% d => d[0] - d[2] %}
-  | MD {% d => d[0] %}
 
 N ->
     [0-9]:+ {% d => parseInt(d[0].join(''), 10) %}
