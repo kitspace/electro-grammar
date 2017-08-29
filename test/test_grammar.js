87826e2c35083f8908f42fc93f83c5c521f200b9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-08-29 20:15:59 +0100

    Add test for multiple decimal points

--- test/test_grammar.js
+++ test/test_grammar.js
@@ -4,46 +4,53 @@ const assert  = require('assert')
 const grammar = require('../lib/grammar')
 
 describe('grammar', () => {
+  let p
+  beforeEach(() => {
+    p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
+  })
+
   it('parses', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('1uF 0603')
     assert(p.results.length > 0, 'has no result')
   })
 
   it('parses tolerance', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('1uF 0603 25%')
     assert(p.results.length > 0, 'has no result')
   })
 
   it('parses tolerance 2', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('1uF 0603 +/-30%')
     assert(p.results.length > 0, 'has no result')
   })
 
   it('parses tolerance 3', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('1uF 0603 ±10%')
     assert(p.results.length > 0, 'has no result')
   })
 
   it('can have things in any order', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('25% 0603 1uF')
     assert(p.results.length > 0, 'has no result')
   })
 
   it('can have things in any order 2', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('25% 1uF 0603')
     assert(p.results.length > 0, 'has no result')
   })
 
   it('can have things in any order 3', () => {
-    const p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
     p.feed('1uF 25% 0603')
     assert(p.results.length > 0, 'has no result')
+  })
+
+  it('rejects multiple decimal points', () => {
+    try {
+      p.feed('1.2.3uF 25% 0603')
+    } catch (e) {
+      assert(e.message.match(/Unexpected/), 'got a different error message')
+    }
+    assert(p.results == null, 'got a result')
   })
 
 })
