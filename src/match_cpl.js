4636fd82170ce7ba2ee1e8dcaf93ac42285edaba
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-08-30 00:46:37 +0100

    Implement CPL matching

--- src/match_cpl.js
+++ src/match_cpl.js
@@ -5,27 +5,37 @@ function matchCPL(component) {
   if (component.capacitance != null) {
     return matchCapacitor(component)
   } else if (component.resistance != null) {
-    let r =  matchResistor(component)
-    console.log(r)
-    return r
+    return matchResistor(component)
   }
   return []
 }
 
 function matchResistor(c) {
-  return resistors.reduce((prev, cpl_part) => {
-    const resistance = cpl_part.resistance === c.resistance
-    const size       = cpl_part.size === c.size
-    const tolerance  = cpl_part.tolerance >= c.tolerance
+  return resistors.reduce((prev, cpl) => {
+    const resistance = cpl.resistance === c.resistance
+    const size       = c.size == null || cpl.size === c.size
+    const tolerance  = c.tolerance == null || cpl.tolerance <= c.tolerance
     if (resistance && size && tolerance) {
-      return prev.concat([cpl_part.cplid])
+      return prev.concat([cpl.cplid])
     }
     return prev
   }, [])
 }
 
 function matchCapacitor(c) {
-  return []
+  return capacitors.reduce((prev, cpl) => {
+    const capacitance = cpl.capacitance === c.capacitance
+    const size        = c.size == null || cpl.size === c.size
+    const characteristic = c.characteristic == null
+      || cpl.characteristic === c.characteristic
+    const tolerance = c.tolerance == null || cpl.tolerance <= c.tolerance
+    const voltage_rating = c.voltage_rating == null
+      || cpl.voltage_rating >= c.voltage_rating
+    if (capacitance && size && characteristic && tolerance && voltage_rating) {
+      return prev.concat([cpl.cplid])
+    }
+    return prev
+  }, [])
 }
 
 module.exports = matchCPL
