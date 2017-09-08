7c4e9ea6b5d8ab3cd63452980ccbfad5632b079f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-09-08 20:25:18 +0100

    Adjust lexing

--- src/parse.js
+++ src/parse.js
@@ -7,18 +7,19 @@ function parse(str) {
     grammar.ParserStart,
     {keepHistory: true}
   )
-  const words = str.split(' ')
+  const words = str.split(/;|,| /)
+    .filter(word => word !== '')
+    .map(word => word + ' ')
   let info = parser.save()
   return words.reduce((prev, word) => {
-    word = word.replace(/,|;/, '') + ' '
-    //if it fails, roll it back
+    // if it fails, roll it back
     try {
       parser.feed(word)
     } catch(e) {
       parser.restore(info)
     }
     info = parser.save()
-    //return the latest valid result
+    // return the latest valid result
     return parser.results[0] || prev
   }, {})
 }
