2ced85371c6d2c18e016b023cd541e67bb68bafa
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-10-15 19:21:10 +0100

    Update lark and re-build

--- python/electro_grammar.py
+++ python/electro_grammar.py
@@ -1,6 +1,6 @@
 from lark import Lark
-from grammar import grammar, TranformNearley
+from grammar import grammar, TransformNearley
 
 parser = Lark(grammar, start="n_main", parser='earley', lexer=None, earley__all_derivations=False)
 def parse(text):
-    return TranformNearley().transform(parser.parse(text))
+    return TransformNearley().transform(parser.parse(text))
