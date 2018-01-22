6f513a9f34f1ee0ad38194b391658dd4f53d50b8
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-22 01:09:57 +0000

    Start ignorer error listeners in lax_parser

--- js/lib/lax_parser.js
+++ js/lib/lax_parser.js
@@ -17,17 +17,47 @@ class ElectroGrammarToObjectListener extends ElectroGrammarListener {
   }
 }
 
+class LexerIgnoreListener extends antlr4.error.ErrorListener {
+  constructor() {
+    super();
+    this.ignored = '';
+  }
+  syntaxError(lexer, offendingSymbol, line, char, err) {
+    // hopefully there is a better way to get the input, but I haven't found it
+    let input = err.split(':')[1].trim();
+    input = input.substring(1, input.length - 1);
+    this.ignored += input;
+  }
+}
+
+class ParserIgnoreListener extends antlr4.error.ErrorListener {
+  constructor() {
+    super();
+    this.ignored = '';
+  }
+  syntaxError(lexer, offendingSymbol, line, char, err) {
+    this.ignored += offendingSymbol.getText();
+  }
+}
+
 function parse(input) {
   const chars = new antlr4.InputStream(input);
   const lexer = new ElectroGrammarLexer(chars);
+  const lexerIgnorer = new LexerIgnoreListener();
+  lexer.removeErrorListeners();
+  lexer.addErrorListener(lexerIgnorer);
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new ElectroGrammarParser(tokens);
+  const parserIgnorer = new ParserIgnoreListener();
   parser.buildParseTrees = true;
+  parser.removeErrorListeners();
+  parser.addErrorListener(parserIgnorer);
 
   const tree = parser.electro_grammar();
   const listener = new ElectroGrammarToObjectListener();
   const walker = antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-  return listener.obj;
+  console.log({lexer: lexerIgnorer.ignored, parser: parserIgnorer.ignored});
+  return {component: listener.obj, ignored: ''};
 }
 
 module.exports = {parse};
