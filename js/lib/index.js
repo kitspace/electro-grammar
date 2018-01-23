d663ca3c6374084810aab14863480ccbc8d0e626
David Craven <david@craven.ch>
2018-01-23 16:03:17 +0100

    js: Feature parity with python port.

--- js/lib/index.js
+++ js/lib/index.js
@@ -4,113 +4,114 @@ const {ElectroGrammarListener} = require('./ElectroGrammarListener');
 const antlr4 = require('antlr4');
 
 function handle_unit(ctx, prefix) {
-  return  Number(ctx.NUMBER().getText + prefix);
+  return  Number(ctx.NUMBER().getText() + prefix);
 }
 
 function handle_prefix(ctx) {
-  if (ctx.hasAttribute('GIGA') && ctx.GIGA()) {
+  if ('GIGA' in ctx && ctx.GIGA()) {
     return 'e9';
   }
-  if (ctx.hasAttribute('MEGA') && ctx.MEGA()) {
+  if ('MEGA' in ctx && ctx.MEGA()) {
     return 'e6';
   }
-  if (ctx.hasAttribute('KILO') && ctx.KILO()) {
+  if ('KILO' in ctx && ctx.KILO()) {
     return 'e3';
   }
-  if (ctx.hasAttribute('MILI') && ctx.MILI()) {
+  if ('MILI' in ctx && ctx.MILI()) {
     return 'e-3';
   }
-  if (ctx.hasAttribute('MICRO') && ctx.MICRO()) {
+  if ('MICRO' in ctx && ctx.MICRO()) {
     return 'e-6';
   }
-  if (ctx.hasAttribute('NANO') && ctx.NANO()) {
+  if ('NANO' in ctx && ctx.NANO()) {
     return 'e-9';
   }
-  if (ctx.hasAttribute('PICO') && ctx.PICO()) {
+  if ('PICO' in ctx && ctx.PICO()) {
     return 'e-12';
   }
   return '';
 }
 
 function handle_package_size(ctx) {
-  if (ctx.hasAttribute('METRIC_SIZE') && ctx.METRIC_SIZE()) {
-    return ctx.METRIX_SIZE().getText;
+  if ('METRIC_SIZE' in ctx && ctx.METRIC_SIZE()) {
+    return ctx.METRIC_SIZE().getText();
   }
-  if (ctx.hasAttribute('IMPERIAL_SIZE') && ctx.IMPERIAL_SIZE()) {
-    return ctx.IMPERIAL_SIZE().getText;
+  if ('IMPERIAL_SIZE' in ctx && ctx.IMPERIAL_SIZE()) {
+    return ctx.IMPERIAL_SIZE().getText();
   }
-  if (ctx.hasAttribute('AMBIGUOUS_SIZE') && ctx.AMBIGUOUS_SIZE()) {
-    return ctx.AMBIGUOUS_SIZE().getText;
+  if ('AMBIGUOUS_SIZE' in ctx && ctx.AMBIGUOUS_SIZE()) {
+    return ctx.AMBIGUOUS_SIZE().getText();
   }
+  return '';
 }
 
 class ElectroGrammarToObjectListener extends ElectroGrammarListener {
   constructor() {
     super();
     this.obj = {};
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitVprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitVoltage(ctx) {
     this.obj['voltage'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitAprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitCurrent(ctx) {
     this.obj['current'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitPprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitPower(ctx) {
     this.obj['power'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitRprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitResistance(ctx) {
     this.obj['resistance'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitCprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitCapacitance(ctx) {
     this.obj['capacitance'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitLprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitInductance(ctx) {
     this.obj['inductance'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitFprefix(ctx) {
     this.prefix = handle_prefix(ctx);
   }
   exitFrequency(ctx) {
     this.obj['frequency'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitTprefix(ctx) {
     this.prefix = handle_prefix(ctx, this.prefix);
   }
   exitTime(ctx) {
     this.obj['time'] = handle_unit(ctx, this.prefix);
-    this.prefix = 1;
+    this.prefix = '';
   }
   exitTemperature(ctx) {
     this.obj['temperature'] = handle_unit(ctx, '');
   }
   exitTolerance(ctx) {
-    this.obj['tolerance'] = handle_unit(ctx);
+    this.obj['tolerance'] = handle_unit(ctx, '');
   }
   exitMetric_size(ctx) {
     var imperial_lookup = {
@@ -179,7 +180,7 @@ class ElectroGrammarToObjectListener extends ElectroGrammarListener {
     this.obj['dielectric'] = dielectric;
   }
   exitClass2(ctx) {
-    this.obj['dielectric'] = ctx.CLASS2().getText.upper();
+    this.obj['dielectric'] = ctx.CLASS2().getText().toUpperCase();
   }
   exitAlu(ctx) {
     this.obj['dielectric'] = 'ALU';
@@ -207,7 +208,7 @@ class ElectroGrammarToObjectListener extends ElectroGrammarListener {
     this.obj['type'] = 'diode';
   }
   exitDcode(ctx) {
-    this.obj['code'] = ctx.DCODE().getText.upper();
+    this.obj['code'] = ctx.DCODE().getText().toUpperCase();
   }
   exitSignal(ctx) {
     this.obj['dtype'] = 'signal';
@@ -225,39 +226,46 @@ class ElectroGrammarToObjectListener extends ElectroGrammarListener {
     this.obj['dtype'] = 'zener';
   }
   exitColor(ctx) {
-    this.obj['color'] = ctx.COLOR().getText.lower();
+    this.obj['color'] = ctx.COLOR().getText().toLowerCase();
   }
   exitTransistor(ctx) {
     this.obj['type'] = 'transistor';
   }
   exitTtype(ctx) {
-    this.obj['ttype'] = ctx.TTYPE().getText.lower();
+    this.obj['ttype'] = ctx.TTYPE().getText().toLowerCase();
   }
   exitTcode(ctx) {
-    this.obj['code'] = ctx.TCODE().getText.upper();
+    this.obj['code'] = ctx.TCODE().getText().toUpperCase();
   }
+}
+
+function get_parser(start_rule) {
+  function parse(input) {
+    const chars = new antlr4.InputStream(input);
+    const lexer = new ElectroGrammarLexer(chars);
+    const tokens = new antlr4.CommonTokenStream(lexer);
+    const parser = new ElectroGrammarParser(tokens);
+
+    // enable grammar ambiguity diagnostic output
+    // see Antlr book ch 9.2, page 156
+    // https://github.com/antlr/antlr4/issues/2206
+    parser.removeErrorListeners();
+    parser.addErrorListener(new antlr4.error.DiagnosticErrorListener());
+    parser._interp.PredictionMode =
+      antlr4.atn.PredictionMode.LL_EXACT_AMBIG_DETECTION;
+
+    parser.buildParseTrees = true;
+
+    const tree = parser[start_rule]();
+    const listener = new ElectroGrammarToObjectListener();
+    const walker = antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
+    return listener.obj;
+  }
+  return parse;
 }
 
 function parse(input) {
-  const chars = new antlr4.InputStream(input);
-  const lexer = new ElectroGrammarLexer(chars);
-  const tokens = new antlr4.CommonTokenStream(lexer);
-  const parser = new ElectroGrammarParser(tokens);
-
-  // enable grammar ambiguity diagnostic output
-  // see Antlr book ch 9.2, page 156
-  // https://github.com/antlr/antlr4/issues/2206
-  parser.removeErrorListeners();
-  parser.addErrorListener(new antlr4.error.DiagnosticErrorListener());
-  parser._interp.PredictionMode =
-    antlr4.atn.PredictionMode.LL_EXACT_AMBIG_DETECTION;
-
-  parser.buildParseTrees = true;
-
-  const tree = parser.electro_grammar();
-  const listener = new ElectroGrammarToObjectListener();
-  const walker = antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-  return listener.obj;
+  return get_parser('electro_grammar')(input);
 }
 
-module.exports = {parse};
+module.exports = {get_parser, parse};
