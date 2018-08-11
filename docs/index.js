ee87ec952d2ed60673370a7f7a2fcf7d58bbfcb0
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-08-11 14:04:34 +0100

    Add CPL links to demo

--- docs/index.js
+++ docs/index.js
@@ -10,7 +10,19 @@ var cplids    = document.getElementById('cplids')
 function setOutput() {
   var c = parse(input.value || input.placeholder)
   component.innerHTML = JSON.stringify(c, null, 2)
-  cplids.innerHTML    = JSON.stringify(matchCPL(c), null, 2)
+  var cplParts = matchCPL(c)
+  while (cplids.hasChildNodes()) {
+      cplids.removeChild(cplids.lastChild);
+  }
+  cplParts.forEach(function(p) {
+    var li = document.createElement('li')
+    var a = document.createElement('a')
+    a.href = 'https://octopart.com/common-parts-library#' + p
+    a.appendChild(document.createTextNode(p))
+    li.appendChild(a)
+    cplids.appendChild(li)
+  })
+
 }
 
 setOutput()
