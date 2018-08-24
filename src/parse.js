2da6aa9602f5e844965184d9ef19698bca53db81
Steve Perkins <steve@octopart.com>
2018-08-24 15:20:06 -0400

    collect unmatched tokens

--- src/parse.js
+++ src/parse.js
@@ -9,18 +9,23 @@ function parse(str) {
   )
   const words = str.split(' ')
   let info = parser.save()
-  return words.reduce((prev, word) => {
+  let unmatched = []
+  let matched = words.reduce((prev, word) => {
     word = word.replace(/,|;/, '') + ' '
     //if it fails, roll it back
     try {
       parser.feed(word)
     } catch(e) {
+      unmatched.push(word.trim())
       parser.restore(info)
     }
     info = parser.save()
     //return the latest valid result
     return parser.results[0] || prev
   }, {})
+
+  matched.unmatched = unmatched
+  return matched
 }
 
 module.exports = parse
